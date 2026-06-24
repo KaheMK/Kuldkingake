78651e994d5293732681b9320b4b1da676daf2f4
KaheM <kaarelkaarle@gmail.com>
2026-06-24 14:15:21 +0300

    chore(archive): stub js/config.js (archived)

--- js/config.js
+++ js/config.js
@@ -1,28 +1,3 @@
-// Konfiguratsioonifail. Asenda KATASTER_WMS.url ametliku WMS/WMTS URL-iga.
-// Kui kasutad Maaameti WMTS, lisa vastav WMTS URL ja parameetrid.
-
-export const MAAMET_HYBRID = {
-  // Näidis: Esri WorldImagery + label overlay creates hybrid look.
-  // Kui sul on Maaameti hübriid WMTS, asenda siia selle URL.
-  satelliteUrl: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-  labelUrl: 'https://stamen-tiles.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}.png'
-};
-
-export const KATASTER_WMS = {
-  // Asenda see reaalse Maaameti WMS URL-iga, nt 'https://kaart.maaamet.ee/wms/alus'
-  url: 'https://kaart.maaamet.ee/wms/alus',
-  layer: 'KATASTRIYKSUS'
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Archived: original content moved to refactor/clean-skeleton archive folder.
+// See archive/README.md for details.
+export default {};
